af66fa40758ed3dfd7cead8db5803d237878e807
asrorxongiyosov <asrorxongiyosov@gmail.com>
2022-11-19 21:25:33 +0500

    database-connection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import Nav from "react-bootstrap/esm/Nav";
 import { useContext } from "react";
 import { Store } from "./Store";
 import CartScreen from "./screens/CartScreen";
+import SigninScreen from "./screens/SigninScreen";
 
 
 function App() {
@@ -42,6 +43,7 @@ function App() {
             <Routes>
               <Route path="/product/:slug" element={<ProductScreen />} />
               <Route path="/cart" element={<CartScreen />} />
+              <Route path="signin" element={<SigninScreen />} />
               <Route path="/" element={<HomeScreen />} />
             </Routes>
           </Container>
